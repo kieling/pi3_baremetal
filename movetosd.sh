b55cc8a1778dc79396ba670d0502e9ee735aa783
Vitor Kieling <vitorkieling@gmail.com>
2016-05-10 02:37:07 +0200

    Changes to makefile (still needs work), simple script to push to sdcard.

---
+++ movetosd.sh
@@ -0,0 +1,4 @@
+cp -rf build/kernel.elf   ~/../../run/media/kieling/9016-4EF8/
+cp -rf build/kernel.img   ~/../../run/media/kieling/9016-4EF8/
+cp -rf boot/start.elf     ~/../../run/media/kieling/9016-4EF8/
+cp -rf boot/bootcode.bin  ~/../../run/media/kieling/9016-4EF8/
